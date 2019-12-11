661ccdc26f3d808b0f838cd08eacd24ed86a71f9
Omar Younis <oimyounis@gmail.com>
2019-12-11 16:45:50 +0300

    preserve for testing

--- main/main.go
+++ main/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"gott"
+	"gott/bytes"
+	"log"
 )
 
 func main() {
+	log.Println(bytes.BinaryStringToByte("0011"))
+	//gott.MakePublishPacket([]byte("aa/b"), []byte(""), 1, 1, 1)
+	//gott.MakePublishPacket([]byte("aa/b"), []byte(""), 1, 0, 1)
+	//gott.MakePublishPacket([]byte("a/b"), []byte(""), 0, 2, 0)
 	broker := gott.NewBroker(":1883")
 	if err := broker.Listen(); err != nil {
 		panic(err)
