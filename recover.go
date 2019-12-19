99737310a0a78134a1c84e5cd365a6c2e4ffa16b
Omar Younis <oimyounis@gmail.com>
2019-12-19 15:48:01 +0300

    enable stack printing

--- recover.go
+++ recover.go
@@ -1,13 +1,16 @@
 package gott
 
 import (
+	"fmt"
 	"log"
+	"runtime/debug"
 )
 
 func Recover() {
 	if r := recover(); r != nil {
 		log.Println("Recovered from panic:", r)
-		//stack := string(debug.Stack())
+		stack := string(debug.Stack())
+		fmt.Println(stack)
 		// TODO: report panic to remote service and log to file
 	}
 }
