3de5e31c3a4e5a5453de6a9433c4af51d94bcb2f
Omar Younis <oimyounis@gmail.com>
2019-12-20 15:50:47 +0300

    add optional callback to Recover func

--- recover.go
+++ recover.go
@@ -6,11 +6,15 @@ import (
 	"runtime/debug"
 )
 
-func Recover() {
+func Recover(callback func(err, stack string)) {
 	if r := recover(); r != nil {
 		log.Println("Recovered from panic:", r)
 		stack := string(debug.Stack())
 		fmt.Println(stack)
 		// TODO: report panic to remote service and log to file
+
+		if callback != nil {
+			callback(fmt.Sprintf("%s", r), stack)
+		}
 	}
 }
